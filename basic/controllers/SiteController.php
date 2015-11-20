8e7136130d4838b5f740723c7f37af96b6a832f4
stas <potapenko.stanislav@gmail.com>
2015-11-20 22:40:21 +0300

    returned users login functionality

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -3,8 +3,10 @@
 namespace app\controllers;
 
 use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
+use app\models\LoginForm;
 
 /**
  * Class SiteController
@@ -19,6 +21,17 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -27,6 +40,29 @@ class SiteController extends Controller
             ],
         ];
     }
+
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+          'model' => $model,
+        ]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
 
     /**
      * @return array
--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -40,6 +40,13 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Books', 'url' => ['/book/index']],
             ['label' => 'Author', 'url' => ['/author/index']],
+          Yii::$app->user->isGuest ?
+            ['label' => 'Login', 'url' => ['/site/login']] :
+            [
+              'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+              'url' => ['/site/logout'],
+              'linkOptions' => ['data-method' => 'post']
+            ],
         ],
     ]);
     NavBar::end();
