7f538568a246b4965a2149a3c79b9a51e5292ce8
stas <potapenko.stanislav@gmail.com>
2015-11-22 22:26:11 +0300

    Search field to inline view

--- basic/views/book/_search.php
+++ basic/views/book/_search.php
@@ -24,40 +24,41 @@ use yii\jui\DatePicker;
         <div class="col-md-3">
             <?php echo $form->field($model, 'author_fullname')->dropDownList(
                 ArrayHelper::map(Author::find()->all(), 'id', 'fullname'),
-                ['prompt' => 'Choose...']
-            ) ?>
+                ['prompt' => $model->getAttributeLabel('author_fullname')]
+            )->label(false) ?>
         </div>
         <div class="col-md-3">
-            <?php echo $form->field($model, 'name') ?>
+            <?php echo $form->field($model, 'name', [
+              'inputOptions' => [
+                'placeholder' => $model->getAttributeLabel('name'),
+              ],
+            ])->label(false);
+            ?>
+
         </div>
     </div>
-
-    <div class="form-group row">
-        <div class="col-md-3">
+    <div class="form-inline">
+    <div class="form-group">
             <?php echo $form->field($model, 'date_from')->widget(
                 DatePicker::classname(), [
                 'dateFormat' => 'yyyy-MM-dd',
             ]
             ) ?>
-        </div>
-        <div class="col-md-3">
             <?php echo $form->field($model, 'date_to')->widget(
                 DatePicker::classname(), [
                 'dateFormat' => 'yyyy-MM-dd',
             ]
             ) ?>
-        </div>
-
-        <div class="right" style="margin-top:20px; float:right;">
+    </div>
+        <div class="right" style="float:right;">
             <?= Html::submitButton(
-                Yii::t('app', 'Search'), ['class' => 'btn btn-primary']
+              Yii::t('app', 'Search'), ['class' => 'btn btn-primary']
             ) ?>
             <?= Html::a(
-                Yii::t('app', 'Reset'), '#',
-                ['class' => 'btn btn-default btn-reset']
+              Yii::t('app', 'Reset'), '#',
+              ['class' => 'btn btn-default btn-reset']
             ) ?>
         </div>
-
     </div>
 
     <?php ActiveForm::end(); ?>
